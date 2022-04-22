71582bf65f4132783331ee7d4d851de2909f558d
sushant <sush@gmail.com>
2022-04-22 12:24:29 +0530

    first commit

---
+++ lib/RecipeView.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+class RecipeView extends StatefulWidget {
+
+String url;
+RecipeView(this.url);
+  @override
+  _RecipeViewState createState() => _RecipeViewState();
+}
+
+class _RecipeViewState extends State<RecipeView> {
+  late  String finalUrl;
+  final Completer<WebViewController> controller = Completer<WebViewController>();
+  @override
+  void initState() {
+    if(widget.url.toString().contains("http://"))
+      {
+        finalUrl = widget.url.toString().replaceAll("http://", "https://");
+      }
+    else{
+      finalUrl = widget.url;
+    }
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Food Recipe App"),
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: finalUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController){
+            setState(() {
+              controller.complete(webViewController);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
